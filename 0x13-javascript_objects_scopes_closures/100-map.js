e8ce1ab1da9d7ac861ed7bf4bb05d66cc0b922fa
wadii2 <zainewadii@gmail.com>
2024-03-12 16:21:25 -0700

    up

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const map = list.map((x, i) => x * i);
+
+const { list } = require('./100-data');
+
+const map1 = list.map((x, i) => x * i);
 console.log(list);
-console.log(map);
+console.log(map1);
